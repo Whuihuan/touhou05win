aaffddeb9751c9c1be75849a72be2fe02b06a67d
wintiger0222 <0xodbs02@naver.com>
2018-06-17 11:21:35 +0900

    false commit

--- resource.h
+++ resource.h
@@ -1,12 +1,13 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by rt5sandbox.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// rt5sandbox.rc에서 사용되고 있습니다.
+//
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
